4b5fbef6d7e5283f1b7ac2872d178f91b0207f65
Brad London and Michelle Brecunier <london.brad@gmail.com>
2016-02-10 16:21:56 -0800

    add user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,29 @@ import org.apache.commons.lang.StringUtils;
 
 public class App {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/main.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/results.vtl");
+
+      String userString = request.queryParams("userString");
+      String userFind = request.queryParams("userFind");
+      String userReplace = request.queryParams("userReplace");
+      String changedString = findAndReplace(userString, userFind, userReplace);
+
+      model.put("userString", userString);
+      model.put("changedString", changedString);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 
   public static String findAndReplace(String userString, String userFind, String userReplace) {
